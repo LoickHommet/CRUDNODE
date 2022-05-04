92724d34d8efe74fc09a445d0be32c59f078b069
Loick Hommet <hommet.loick@gmail.com>
2022-05-04 10:35:11 +0200

    route api/name/1  avoir une seul personne

--- server.js
+++ server.js
@@ -8,19 +8,24 @@ memoryDb.set(id++, { nom: "Bob" });
 memoryDb.set(id++, { nom: "Charlie" });
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/api/names") {
-        if (req.method === "GET") {
-    
-            let data = "";
-            data = JSON.stringify(Array.from(memoryDb.entries()));
-            res.writeHead(200, { "content-type": "application/json" });
-            res.write(data);
-           
-
-        }
-        
+  if (req.url === "/api/names") {
+    if (req.method === "GET") {
+        let data = "";
+        data = JSON.stringify(Array.from(memoryDb.entries()));
+        res.writeHead(200, { "content-type": "application/json" });
+        res.write(data);
     }
-    res.end()
+  } else if (req.url.includes("/api/name/")) {
+        let data = "";
+        const arrayReq = req.url.split("/");
+        const id = parseInt(arrayReq[arrayReq.length - 1]);
+        console.log(id);
+        data = JSON.stringify(memoryDb.get(id));
+
+        res.writeHead(200, { "content-type": "application/json" });
+        res.write(data);
+  }
+  res.end();
 });
 
 server.listen(4000);
