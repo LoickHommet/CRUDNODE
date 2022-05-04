256e82fafc7d4051d4a2c000ff4d60eccd0a4781
Loick Hommet <hommet.loick@gmail.com>
2022-05-04 13:48:46 +0200

    Ajoue method delete

--- server.js
+++ server.js
@@ -16,22 +16,53 @@ const server = http.createServer((req, res) => {
       res.write(data);
     }
     if (req.method === "POST") {
-        let data = '';
-        req.on('data', chunk => {
-            data += chunk;
-        });
-        req.on('end', () => {
-            // INCLURE VOTRE LOGIQUE DE ROUTE ICI
+      let data = "";
+      req.on("data", (chunk) => {
+        data += chunk;
+      });
+      req.on("end", () => {
+        // INCLURE VOTRE LOGIQUE DE ROUTE ICI
 
-            // ici vous récupérez le JSON sous forme d'un objet Javascript 
-            data = JSON.parse(data); 
-            memoryDb.set(++id, data);
+        // ici vous récupérez le JSON sous forme d'un objet Javascript
+        data = JSON.parse(data);
+        memoryDb.set(++id, data);
 
-            res.end();
-        });
-
+        res.end();
+      });
     }
-  } 
+
+    if (req.method === "DELETE") {
+        const arrayReq = req.url.split("/");
+        const id = parseInt(arrayReq[arrayReq.length - 1]);
+
+        if (id) {
+            if (memoryDb.has(id) != null) {
+                memoryDb.delete(id);
+            }
+
+            else { // Si l'utilisateur n'existe pas
+                res.writeHead(404, { 'content-type': "text/html" });
+                res.write("<h1>Cet utilisateur n'existe pas</h1>");
+            }
+        }
+
+        else { // Si aucun id n'a été entré
+            res.writeHead(404, { 'content-type': "text/html" });
+            res.write("<h1>Cet utilisateur n'existe pas</h1>");
+        }
+    }
+  } else if (req.url.includes("/api/name/")) {
+    let data = "";
+    const arrayReq = req.url.split("/");
+    const id = parseInt(arrayReq[arrayReq.length - 1]);
+
+    data = JSON.stringify(memoryDb.get(id));
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.write(data);
+  }
+
+  
   res.end();
 });
 
