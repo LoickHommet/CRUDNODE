170e84b577772361dea9ea7fc61ef5d6f6a04f61
Loick Hommet <hommet.loick@gmail.com>
2022-05-04 11:19:10 +0200

    ajoue d'un route post

--- server.js
+++ server.js
@@ -10,21 +10,28 @@ memoryDb.set(id++, { nom: "Charlie" });
 const server = http.createServer((req, res) => {
   if (req.url === "/api/names") {
     if (req.method === "GET") {
-        let data = "";
-        data = JSON.stringify(Array.from(memoryDb.entries()));
-        res.writeHead(200, { "content-type": "application/json" });
-        res.write(data);
+      let data = "";
+      data = JSON.stringify(Array.from(memoryDb.entries()));
+      res.writeHead(200, { "content-type": "application/json" });
+      res.write(data);
     }
-  } else if (req.url.includes("/api/name/")) {
-        let data = "";
-        const arrayReq = req.url.split("/");
-        const id = parseInt(arrayReq[arrayReq.length - 1]);
-        console.log(id);
-        data = JSON.stringify(memoryDb.get(id));
+    if (req.method === "POST") {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        });
+        req.on('end', () => {
+            // INCLURE VOTRE LOGIQUE DE ROUTE ICI
 
-        res.writeHead(200, { "content-type": "application/json" });
-        res.write(data);
-  }
+            // ici vous récupérez le JSON sous forme d'un objet Javascript 
+            data = JSON.parse(data); 
+            memoryDb.set(++id, data);
+
+            res.end();
+        });
+
+    }
+  } 
   res.end();
 });
 
